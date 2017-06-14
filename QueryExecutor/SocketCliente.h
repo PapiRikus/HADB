2ce891459805734cbe4d89fc45e2f8852844a32f
Robert <robert19972008@hotmail.com>
2017-06-14 02:15:33 -0600

    Query Executor 1.0
    Actualizacion

---
+++ QueryExecutor/SocketCliente.h
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   SocketCliente.h
+ * Author: cp
+ *
+ * Created on 13 de junio de 2017, 05:13 PM
+ */
+
+#ifndef SOCKETCLIENTE_H
+#define SOCKETCLIENTE_H
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <string.h>
+#include <string>
+#include <iostream>
+#include <pthread.h>
+#include <vector>
+#include <unistd.h>
+#include <arpa/inet.h>
+//#include<qt5/QtCore/QObject>
+using namespace std;
+
+
+class SocketCliente {
+public:
+    SocketCliente();
+    SocketCliente(const SocketCliente& orig);
+    virtual ~SocketCliente();
+    bool conectar();
+    void setMensaje(const char *msj);
+private:
+    int descriptor;
+    sockaddr_in info;
+    bool crear_Socket;
+    static void *controlador(void*obj);
+    
+};
+
+#endif /* SOCKETCLIENTE_H */
+
---
+++ QueryExecutor/build/Debug/GNU-Linux/ValidarConsulta.o.d
@@ -0,0 +1,4 @@
+build/Debug/GNU-Linux/ValidarConsulta.o: ValidarConsulta.cpp \
+ ValidarConsulta.h
+
+ValidarConsulta.h:
