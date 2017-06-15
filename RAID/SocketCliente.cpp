c8cda2c5a996efb1c8058ced4ace4026bd9cd98a
Unknown <noticiasnuevayo@gmail.com>
2017-06-15 11:22:19 -0600

    version con funciones json
    Se agregan metodos de prueba

--- RAID/SocketCliente.cpp
+++ RAID/SocketCliente.cpp
@@ -16,8 +16,8 @@ bool SocketCliente::conectar(){
     if(descriptor < 0)
         return false;
     info.sin_family = AF_INET;
-    info.sin_addr.s_addr = inet_addr("192.168.1.132");
-    info.sin_port = ntohs(30667);
+    info.sin_addr.s_addr = inet_addr("127.0.0.1");
+    info.sin_port = ntohs(30666);
     memset(&info.sin_zero,0,sizeof(info.sin_zero));
 
     if (connect(descriptor,(sockaddr*)&info,(socklen_t)sizeof(info))<0){
@@ -28,7 +28,7 @@ bool SocketCliente::conectar(){
     pthread_create(&hilo,0,SocketCliente::controlador,(void*)this);
     pthread_detach(hilo);
     
-    send(descriptor,"DN",strlen("DN"),0);
+    //send(descriptor,"DN",strlen("DN"),0);
 
     return true;
 }
@@ -59,26 +59,95 @@ void * SocketCliente::controlador(void*obj){
         } else{
             string accion = root.get("Accion", "A Default Value if not exists" ).asString().c_str();
             cout<<"Accion "<<accion;
-            string table  = root.get("Tabla", "A Default Value if not exists" ).asString().c_str();
-            int tabla = atoi(table.c_str());
-            cout<<"Esti es tablka:"<<tabla;
+
             if(accion == "S"){
+                string table  = root.get("Tabla", "A Default Value if not exists" ).asString().c_str();
+                int tabla = atoi(table.c_str());
+                cout<<"Esti es tablka:"<<tabla;
                 cout<<"json valido";
                 long x = datos.crearTabla(tabla);
-                datos.agregarDatoColumna(tabla,x,1,"11010101011111101");
+                //datos.agregarDatoColumna(tabla,x,1,"11010101011111101");
                 //cout <<endl<< x<<endl;
-                cout<<endl<<"Dato recuperado del 'disco': "<<datos.recuperarCaracteres(1 + 64*x + 20480*(x-1) );
+                //cout<<endl<<"Dato recuperado del 'disco': "<<datos.recuperarCaracteres(1 + 64*x + 20480*(x-1) );
                 
                     string retorno = to_string(x);
                     Json::Value fromScratch;
-                    fromScratch["accion"]="S";
-                    fromScratch ["tabla"]=retorno;
+                    fromScratch["accion"] = "S";
+                    fromScratch ["Indice"] = retorno;
+                    fromScratch ["code"]  = "OK";
                     Json::StyledWriter writerr;
                     string out=writerr.write(fromScratch);
-                    send(padre->descriptor,retorno.c_str(),strlen(retorno.c_str()),0);
+                    send(padre->descriptor,out.c_str(),strlen(out.c_str()),0);
+                    cout<<out;
+                
+            }else if(accion == "GC"){
+                string table  = root.get("Tabla", "A Default Value if not exists" ).asString().c_str();
+                int tabla = atoi(table.c_str());
+                string column = root.get("Columna", "A Default Value if not exists" ).asString().c_str();
+                int columna = atoi(column.c_str());
+                string dato = root.get("Dato", "A Default Value if not exists" ).asString().c_str();
+                string index = root.get("Indice", "A Default Value if not exists" ).asString().c_str();
+                int indice = atoi(index.c_str());
+                
+                cout<<tabla<<endl<<indice<<endl<<columna;
+                cout<<"Indice: "<<indice;
+                datos.eliminarDatoColumna(tabla,indice,columna);
+                datos.agregarDatoColumna(tabla,indice,columna,dato);
+                cout<<endl<<"Dato recuperado del 'disco': "<<datos.recuperarCaracteres(1 + 64*indice + 20480*(indice-1) );
+            /*
+                    string x = datos.recuperarCaracteres(1 + 64*indice + 20480*(indice-1) );
+                    Json::Value fromScratch;
+                    fromScratch["Accion"] = "GC";
+                    fromScratch ["Tabla"] = table ;
+                    fromScratch ["Columna"] = column;
+                    fromScratch ["Dato"] = x;
+                    fromScratch ["Indice"] = indice;
+                    fromScratch ["code"] = "OK";
+                    Json::StyledWriter writerr;
+                    string out=writerr.write(fromScratch);
+                    send(padre->descriptor,out.c_str(),strlen(out.c_str()),0);
+                    cout<<out;*/
+            }else if(accion == "EF"){
+                string table  = root.get("Tabla", "A Default Value if not exists" ).asString().c_str();
+                int tabla = atoi(table.c_str());
+                string index = root.get("Indice", "A Default Value if not exists" ).asString().c_str();
+                int indice = atoi(index.c_str());
+                datos.EliminarFila(tabla,indice);
+                
+                    string x = datos.recuperarCaracteres(1 + 64*indice + 20480*(indice-1) );
+                    Json::Value fromScratch;
+                    fromScratch["Accion"] = "EF";
+                    fromScratch ["Tabla"] = table ;
+                    fromScratch ["Indice"] = indice;
+                    fromScratch ["code"] = "OK";
+                    Json::StyledWriter writerr;
+                    string out=writerr.write(fromScratch);
+                    send(padre->descriptor,out.c_str(),strlen(out.c_str()),0);
+                    cout<<out;
+            }else if(accion == "EDC"){
+                string table  = root.get("Tabla", "A Default Value if not exists" ).asString().c_str();
+                int tabla = atoi(table.c_str());
+                string column = root.get("Columna", "A Default Value if not exists" ).asString().c_str();
+                int columna = atoi(column.c_str());
+                string index = root.get("Indice", "A Default Value if not exists" ).asString().c_str();
+                int indice = atoi(index.c_str());
+                datos.eliminarDatoColumna(tabla,indice,columna);
+                cout<<endl<<"Dato recuperado del 'disco': "<<datos.recuperarCaracteres(1 + 64*indice + 20480*(indice-1) );
+           
+                    
+                    Json::Value fromScratch;
+                    fromScratch["Accion"] = "EDC";
+                    fromScratch ["Tabla"] = table;
+                    fromScratch["Indice"] = index;
+                    fromScratch["Columna"] = column;
+                    fromScratch["code"] = "OK";
+                    Json::StyledWriter writerr;
+                    string out=writerr.write(fromScratch);
+                    send(padre->descriptor,out.c_str(),strlen(out.c_str()),0);
                     cout<<out;
                 
             }
+            
         }
         
         
---
+++ rAid/build/Debug/GNU-Linux/SocketCliente.o.d
@@ -0,0 +1,6 @@
+build/Debug/GNU-Linux/SocketCliente.o: SocketCliente.cpp SocketCliente.h \
+ Almacenamiento.h
+
+SocketCliente.h:
+
+Almacenamiento.h:
